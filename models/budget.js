35ea7938ad5d8d3f6392a686d0e62db87edf11d4
Rae Hebron <rae.oxford.hebron@gmail.com>
2022-11-11 15:50:51 -0500

    rendered index route

--- models/budget.js
+++ models/budget.js
@@ -1,4 +1,4 @@
-module.exports = [
+budgets = [
     {
       date: "April 1",
       name: "Income",
@@ -70,3 +70,5 @@ module.exports = [
       tags: ["pets"]
     },
   ];
+
+  module.exports = budgets
